fa89616a5a17fa0c41425c8feb78042c65644a49
pch8388 <pch838811@gmail.com>
2020-07-15 00:16:51 +0900

    init

---
+++ src/main/java/toy/study/hard/HardApplication.java
@@ -0,0 +1,13 @@
+package toy.study.hard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardApplication.class, args);
+    }
+
+}
