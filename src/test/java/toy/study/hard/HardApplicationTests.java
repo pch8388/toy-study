fa09a07060eec06aada6c3ef8baaa6fbbf81b2cc
pch8388 <pch838811@gmail.com>
2020-07-15 00:18:02 +0900

    init

---
+++ src/test/java/toy/study/hard/HardApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.study.hard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
